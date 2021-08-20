98cc8389936fd511ba26069241b2e4c53a079364
Techno Celebes <official@techno-celebes.com>
2021-08-20 19:38:27 +0800

    Folder Structure real

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -42,6 +42,94 @@ class Autoload extends AutoloadConfig
 	public $psr4 = [
 		APP_NAMESPACE => APPPATH, // For custom app namespace
 		'Config'      => APPPATH . 'Config',
+
+		'WebRoot' => ROOTPATH . 'modules/Web',
+		'ApiRoot' => ROOTPATH . 'modules/Api',
+		'CronJobRoot' => ROOTPATH . 'modules/CronJobs',
+		'BusinessProcessRoot' => ROOTPATH . 'modules/BusinessProcess',
+		'ApiMobile' => ROOTPATH . 'modules/APIMobile',
+
+		//Regular
+		'Auth' => ROOTPATH . 'modules/Web/Auth',
+		'Admin' => ROOTPATH . 'modules/Web/Admin',
+		'Home' => ROOTPATH . 'modules/Web/Home',
+		'Master' => ROOTPATH . 'modules/Web/Master',
+		'User' => ROOTPATH . 'modules/Web/User',
+
+		//BP Master{
+		'BusinessProcessRoot/Master/EmailMessage' => ROOTPATH . 'modules/BusinessProcess/Master/EmailMessage',
+		//}
+
+		//WEB{
+			//FOR Admin
+			//Admin Regular
+			'AdminpageFolder' => ROOTPATH . 'modules/Web/Admin/Adminpage',
+			'AdminTemplateFolder' => ROOTPATH . 'modules/Web/Admin/Template',
+
+				//Homepage MVC
+				'AdminpageViewCont' => ROOTPATH . 'modules/Web/Admin/Adminpage/Controllers/ViewCont',
+				'AdminpageFuncCont' => ROOTPATH . 'modules/Web/Admin/Adminpage/Controllers/FuncCont',
+				'AdminpageView' => ROOTPATH . 'modules/Web/Admin/Adminpage/Views',
+				'AdminpageModel' => ROOTPATH . 'modules/Web/Admin/Adminpage/Models',
+
+				//HomeTemplate MVC
+				'AdminTemplateViewCont' => ROOTPATH . 'modules/Web/Admin/Template/Controllers/ViewCont',
+				'AdminTemplateFuncCont' => ROOTPATH . 'modules/Web/Admin/Template/Controllers/FuncCont',
+				'AdminTemplateView' => ROOTPATH . 'modules/Web/Admin/Template/Views',
+				'AdminTemplateModel' => ROOTPATH . 'modules/Web/Admin/Template/Models',
+
+			//FOR User
+			//Student Regular
+			'UserpageFolder' => ROOTPATH . 'modules/Web/Student/Adminpage',
+			'UserTemplateFolder' => ROOTPATH . 'modules/Web/Student/Template',
+
+				//Homepage MVC
+				'UserpageViewCont' => ROOTPATH . 'modules/Web/User/Userpage/Controllers/ViewCont',
+				'UserpageFuncCont' => ROOTPATH . 'modules/Web/User/Userpage/Controllers/FuncCont',
+				'UserpageView' => ROOTPATH . 'modules/Web/User/Userpage/Views',
+				'UserpageModel' => ROOTPATH . 'modules/Web/User/Userpage/Models',
+
+				//HomeTemplate MVC
+				'UserTemplateViewCont' => ROOTPATH . 'modules/Web/User/Template/Controllers/ViewCont',
+				'UserTemplateFuncCont' => ROOTPATH . 'modules/Web/User/Template/Controllers/FuncCont',
+				'UserTemplateView' => ROOTPATH . 'modules/Web/User/Template/Views',
+				'UserTemplateModel' => ROOTPATH . 'modules/Web/User/Template/Models',
+
+
+			//FOR AUTH
+			'AuthpageFolder' => ROOTPATH . 'modules/Web/Auth/Authpage',
+			'AuthTemplateFolder' => ROOTPATH . 'modules/Web/Auth/Template',
+
+				//Homepage MVC
+				'AuthpageViewCont' => ROOTPATH . 'modules/Web/Auth/Authpage/Controllers/ViewCont',
+				'AuthpageFuncCont' => ROOTPATH . 'modules/Web/Auth/Authpage/Controllers/FuncCont',
+				'AuthpageView' => ROOTPATH . 'modules/Web/Auth/Authpage/Views',
+				'AuthpageModel' => ROOTPATH . 'modules/Web/Auth/Authpage/Models',
+
+				//HomeTemplate MVC
+				'AuthTemplateViewCont' => ROOTPATH . 'modules/Web/Auth/Template/Controllers/ViewCont',
+				'AuthTemplateFuncCont' => ROOTPATH . 'modules/Web/Auth/Template/Controllers/FuncCont',
+				'AuthTemplateView' => ROOTPATH . 'modules/Web/Auth/Template/Views',
+				'AuthTemplateModel' => ROOTPATH . 'modules/Web/Auth/Template/Models',		
+
+
+			//FOR HOME
+			//Home Regular
+			'HomepageFolder' => ROOTPATH . 'modules/Web/Home/Homepage',
+			'HomeTemplateFolder' => ROOTPATH . 'modules/Web/Home/Template',
+
+				//Homepage MVC
+				'HomepageViewCont' => ROOTPATH . 'modules/Web/Home/Homepage/Controllers/ViewCont',
+				'HomepageFuncCont' => ROOTPATH . 'modules/Web/Home/Homepage/Controllers/FuncCont',
+				'HomepageView' => ROOTPATH . 'modules/Web/Home/Homepage/Views',
+				'HomepageModel' => ROOTPATH . 'modules/Web/Home/Homepage/Models',
+
+				//HomeTemplate MVC
+				'HomeTemplateViewCont' => ROOTPATH . 'modules/Web/Home/Template/Controllers/ViewCont',
+				'HomeTemplateFuncCont' => ROOTPATH . 'modules/Web/Home/Template/Controllers/FuncCont',
+				'HomeTemplateView' => ROOTPATH . 'modules/Web/Home/Template/Views',
+				'HomeTemplateModel' => ROOTPATH . 'modules/Web/Home/Template/Models',
+		//}
 	];
 
 	/**
