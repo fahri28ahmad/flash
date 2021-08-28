8678f0ca29172cdc5c4960669337fbf3514757d5
Techno Celebes <official@techno-celebes.com>
2021-08-28 17:40:12 +0800

    kelupaan database

--- 210821_flashlab.sql
+++ 210821_flashlab.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 24, 2021 at 04:49 AM
+-- Generation Time: Aug 28, 2021 at 11:39 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.13
 
@@ -39,6 +39,55 @@ CREATE TABLE `admins` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `patients`
+--
+
+CREATE TABLE `patients` (
+  `patient_id` int(11) NOT NULL,
+  `no_ktp` varchar(100) NOT NULL,
+  `complete_name` varchar(200) NOT NULL,
+  `address` text NOT NULL,
+  `phone_number` varchar(20) NOT NULL,
+  `birth_date` date NOT NULL,
+  `gender` tinyint(4) NOT NULL,
+  `user_inputter` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `patients`
+--
+
+INSERT INTO `patients` (`patient_id`, `no_ktp`, `complete_name`, `address`, `phone_number`, `birth_date`, `gender`, `user_inputter`) VALUES
+(8, '90128390218312', 'Halut Halut Bujon', 'Jalan Tadulako', '+6282194077126', '2021-08-17', 2, 8),
+(9, '90128390218312', 'ASdasdw', 'sadasd', '2343432', '2021-08-19', 1, 9),
+(10, '234234230218312', 'Narjun', 'Jalan Hah pe bagaya', '+6282194077126', '2021-08-30', 1, 9);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient_package`
+--
+
+CREATE TABLE `patient_package` (
+  `patient_package_id` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `package` smallint(6) NOT NULL,
+  `date_inserted` datetime NOT NULL,
+  `date_swab` datetime NOT NULL,
+  `package_status` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `patient_package`
+--
+
+INSERT INTO `patient_package` (`patient_package_id`, `patient_id`, `package`, `date_inserted`, `date_swab`, `package_status`) VALUES
+(9, 9, 3, '2021-08-27 22:27:03', '2021-08-27 22:27:03', 0),
+(11, 10, 1, '2021-08-27 22:28:21', '2021-08-27 22:28:21', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -50,6 +99,15 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_password`, `user_name`, `user_email`) VALUES
+(7, '$2y$10$nWJrNv.0Bh8SwwepgiGfmeC4RLpHtAquK7RQeTNtCKzND.ogtNBwK', 'asde', 'yuityz@mdsgf.com'),
+(8, '$2y$10$rMUFMAs2L0UWQv3CiH4jo.LFvLRpt7I.pvFWTqQ47IXODfC75y7Pe', 'yuityz@mdsgf.com', 'yuityz@mdsgf.coma'),
+(9, '$2y$10$rrCkvoEIiDK0wTewkotcEerAdY4tsmytR7cTE2XxFaNkqjwuPDRTa', 'yuityz@mdsgf.comas', 'yuityz@mdsgf.comas');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -58,6 +116,21 @@ CREATE TABLE `users` (
 --
 ALTER TABLE `admins`
   ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `patients`
+--
+ALTER TABLE `patients`
+  ADD PRIMARY KEY (`patient_id`),
+  ADD UNIQUE KEY `no_ktp` (`no_ktp`,`user_inputter`),
+  ADD KEY `user_inputter_fk` (`user_inputter`);
+
+--
+-- Indexes for table `patient_package`
+--
+ALTER TABLE `patient_package`
+  ADD PRIMARY KEY (`patient_package_id`),
+  ADD KEY `patient_id` (`patient_id`);
 
 --
 -- Indexes for table `users`
@@ -76,10 +149,38 @@ ALTER TABLE `admins`
   MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `patients`
+--
+ALTER TABLE `patients`
+  MODIFY `patient_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `patient_package`
+--
+ALTER TABLE `patient_package`
+  MODIFY `patient_package_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `patients`
+--
+ALTER TABLE `patients`
+  ADD CONSTRAINT `user_inputter_fk` FOREIGN KEY (`user_inputter`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `patient_package`
+--
+ALTER TABLE `patient_package`
+  ADD CONSTRAINT `patient_package_ibfk_1` FOREIGN KEY (`patient_id`) REFERENCES `patients` (`patient_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
